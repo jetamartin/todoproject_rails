9ef0505dc5605c1ae93afd76154535ae0c47db32
Jet Martin <jetamartin@gmail.com>
2015-02-22 10:16:20 -0800

    including routes.rb file even though no changes made to it...it was however edited..won't to keep things clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :to_dos
   resources :tasks
+
+
   # match('to_dos', {:via => :get, :to => 'to_dos#index'})
   # match('to_dos/:id', {:via => :get, :to => 'to_dos#show'})
   # match('to_dos/:id', {via: [:patch, :put], to: 'to_dos#update'})
