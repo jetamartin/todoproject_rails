97af130632bfedc52e812c94234e858b0339dfa8
Jet Martin <jetamartin@gmail.com>
2015-02-22 13:37:27 -0800

    added changes to allow select option to default to appropriate to_do.id..note it's not quite working yet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :to_dos
+  resources :to_dos do
+    member do
+      get 'new_task'
+    end
+  end
   resources :tasks
-
-
   # match('to_dos', {:via => :get, :to => 'to_dos#index'})
   # match('to_dos/:id', {:via => :get, :to => 'to_dos#show'})
   # match('to_dos/:id', {via: [:patch, :put], to: 'to_dos#update'})
